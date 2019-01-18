42abfc26497c6ff953ebc3d9b7949f9f47ea5a6e
Diogo Silva <diogomcarvalhosilva@gmail.com>
2019-01-18 08:58:39 +0000

    1ºPush

---
+++ ProjetoPDM/Projeto/Projeto/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Projeto.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Projeto
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Bt_Login_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void Bt_Register_Clicked(object sender, EventArgs e)
+        {
+            DBConnection db = new DBConnection();
+
+            Task<string> resultTask = db.RegisterUser("a", "b", "c");
+            string result = await resultTask;
+
+            await DisplayAlert("Result", result, "OK");
+        }
+    }
+}
