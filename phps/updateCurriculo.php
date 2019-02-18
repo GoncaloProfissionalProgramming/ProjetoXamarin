e25ddbc768427848a1795730b8e9101215b2cf87
Diogo Silva <44698736+DiogoCSilva@users.noreply.github.com>
2019-02-18 20:56:40 +0000

    updateCurriculo

--- phps/updateCurriculo.php
+++ phps/updateCurriculo.php
@@ -4,18 +4,19 @@ require_once "var.php";
 $con = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
 
 $statement = mysqli_prepare($con, "UPDATE curriculo SET telemovel = ?, morada = ?, email=?  WHERE id=?");
-mysqli_stmt_bind_param($statement, "s", $id);
+mysqli_stmt_bind_param($statement, "ssss",$telemovel,$morada,$email,$id);
 mysqli_stmt_execute($statement);
 mysqli_stmt_store_result($statement);
-mysqli_stmt_bind_result($statement, $id,$telemovel,$morada,$email);
+mysqli_stmt_bind_result($statement,$telemovel,$morada,$email,$id);
 $response = array();
 $response["success"] = false;
 while(mysqli_stmt_fetch($statement)){
     $response["success"] = true;
-    $response["id"] = $id;
-    $response["telemovel"] = $telemovel;
-    $response["morada"] = $morada;
-    $response["email"] = $email;
+      
+         $response["telemovel"] = $telemovel;
+         $response["morada"] = $morada;
+         $response["email"] = $email;
+         $response["id"] = $id;
    
    
   }
--- phps/var.php
+++ phps/var.php
@@ -1,5 +1,8 @@
 <?php
 
-$id=$_POST["id"];;
+$id=$_POST["id"];
+$email=$_POST["email"];
+$telemovel=$_POST["telemovel"];
+$morada=$_POST["morada"];
 
 ?>
