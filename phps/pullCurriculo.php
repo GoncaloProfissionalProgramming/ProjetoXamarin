9155bd75bcedc3749cb4fb07ead4b6990f214662
Diogo Silva <44698736+DiogoCSilva@users.noreply.github.com>
2019-02-18 19:07:31 +0000

    pullCurriculo arranjado

--- phps/pullCurriculo.php
+++ phps/pullCurriculo.php
@@ -3,20 +3,21 @@ require_once "connection.php";
 require_once "var.php";
 $con = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
 
-
-
-if($_SERVER['REQUEST_METHOD'] == "POST")
-{
-  $statement = mysqli_prepare($con, "SELECT telemovel,morada,email FROM curriculo  where aluno.username=$username");
-} 
-else 
-{
-  $username = null;
-  echo "no username supplied";
-}
-
-    mysqli_stmt_bind_param($statement, "sss",$username);
+    $statement = mysqli_prepare($con, "SELECT * FROM curriculo WHERE id = ? ");
+    mysqli_stmt_bind_param($statement, "s", $idSent);
     mysqli_stmt_execute($statement);
-    mysqli_stmt_close($statement);
-    echo "php response"
+    mysqli_stmt_store_result($statement);
+    mysqli_stmt_bind_result($statement, $id,$telemovel,$morada,$email);
+    $response = array();
+    $response["success"] = false;
+    while(mysqli_stmt_fetch($statement)){
+        $response["success"] = true;
+        $response["id"] = $id;
+		$response["telemovel"] = $telemovel;
+        $response["morada"] = $morada;
+        $response["email"] = $email;
+       
+       
+      }
+    echo json_encode($response);
 ?>
