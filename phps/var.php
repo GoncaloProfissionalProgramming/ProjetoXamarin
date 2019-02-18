4630ccb7cbde428108cf7a133b36def6067c57e5
Gonçalo Cruz <goncalocruzprofissional@outlook.pt>
2019-02-18 21:23:49 +0000

    pullHorario

--- phps/var.php
+++ phps/var.php
@@ -5,4 +5,13 @@ $email=$_POST["email"];
 $telemovel=$_POST["telemovel"];
 $morada=$_POST["morada"];
 
+$turmaId=$_POST["turmaId"];
+$sala=$_POST["sala"];
+$telemovel=$_POST["telemovel"];
+$disciplina=$_POST["disciplina"];
+$inicio=$_POST["inicio"];
+$fim=$_POST["fim"];
+$diaDaSemana=$_POST["diaDaSemana"];
+
+
 ?>
