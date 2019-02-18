7c495e08b18bce8f29a6c67190cee6ce55048aa1
Gonçalo Cruz <goncalocruzprofissional@outlook.pt>
2019-02-18 19:47:00 +0000

    updateCurriculo

--- phps/pullCurriculo.php
+++ phps/pullCurriculo.php
@@ -4,7 +4,7 @@ require_once "var.php";
 $con = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
 
     $statement = mysqli_prepare($con, "SELECT * FROM curriculo WHERE id = ? ");
-    mysqli_stmt_bind_param($statement, "s", $idSent);
+    mysqli_stmt_bind_param($statement, "s", $id);
     mysqli_stmt_execute($statement);
     mysqli_stmt_store_result($statement);
     mysqli_stmt_bind_result($statement, $id,$telemovel,$morada,$email);
@@ -13,7 +13,7 @@ $con = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
     while(mysqli_stmt_fetch($statement)){
         $response["success"] = true;
         $response["id"] = $id;
-		$response["telemovel"] = $telemovel;
+		    $response["telemovel"] = $telemovel;
         $response["morada"] = $morada;
         $response["email"] = $email;
        
---
+++ phps/updateCurriculo.php
@@ -0,0 +1,24 @@
+﻿<?php
+require_once "connection.php";
+require_once "var.php";
+$con = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+
+$statement = mysqli_prepare($con, "UPDATE curriculo SET telemovel = ?, morada = ?, email=?  WHERE id=?");
+mysqli_stmt_bind_param($statement, "s", $id);
+mysqli_stmt_execute($statement);
+mysqli_stmt_store_result($statement);
+mysqli_stmt_bind_result($statement, $id,$telemovel,$morada,$email);
+$response = array();
+$response["success"] = false;
+while(mysqli_stmt_fetch($statement)){
+    $response["success"] = true;
+    $response["id"] = $id;
+    $response["telemovel"] = $telemovel;
+    $response["morada"] = $morada;
+    $response["email"] = $email;
+   
+   
+  }
+echo json_encode($response);
+
+?>
