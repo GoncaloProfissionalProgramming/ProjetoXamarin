3cb83644cde028e3b0c0a12da275d05a74925b53
Gonçalo Cruz <goncalocruzprofissional@outlook.pt>
2019-02-18 16:03:11 +0000

    pullCurriculo

---
+++ phps/pullCurriculo.php
@@ -0,0 +1,22 @@
+﻿<?php
+require_once "connection.php";
+$con = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+
+
+
+if($_SERVER['REQUEST_METHOD'] == "POST")
+{
+  $sql = "SELECT id FROM aluno where username=$_POST["username"]";
+  $statement = mysqli_prepare($con,$sql, "SELECT telemovel,morada,email FROM curriculo  where aluno.id=curriculo.id");
+} 
+else 
+{
+  $username = null;
+  echo "no username supplied";
+}
+
+    mysqli_stmt_bind_param($statement, "sss");
+    mysqli_stmt_execute($statement);
+    mysqli_stmt_close($statement);
+    echo "php response"
+?>
